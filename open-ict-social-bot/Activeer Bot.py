f807c8dad7df2fbcf8e7125421dcca79c6dc6948
fszzl21 <147528525+fszzl21@users.noreply.github.com>
2025-09-12 21:23:46 +0200

    sorry boi ik had .git verwijderd en weer terug gezet

---
+++ open-ict-social-bot/Activeer Bot.py
@@ -0,0 +1,28 @@
+import discord
+import os # default module
+from dotenv import load_dotenv
+
+load_dotenv() # load all the variables from the env file
+bot = discord.Bot()
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is aantjes banaantjes")
+
+@bot.slash_command(name="hello", description="Say hello to the bot")
+async def hello(ctx: discord.ApplicationContext):
+    await ctx.respond("Hey!")
+
+@bot.command()
+async def gtn(ctx):
+    """A Slash Command to play a Guess-the-Number game."""
+
+    await ctx.respond('Guess a number between 1 and 10.')
+    guess = await bot.wait_for('message', check=lambda message: message.author == ctx.author)
+
+    if int(guess.content) == 5:
+        await ctx.send('You guessed it!')
+    else:
+        await ctx.send('Nope, try again.')
+
+bot.run(os.getenv('TOKEN')) # run the bot with the token
---
+++ open-ict-social-bot/README.md
@@ -0,0 +1,14 @@
+
+# OPEN-ICT SOCIAL BOT
+
+
+Voor ons project hebben wij een bot ontwikkeld om het aspect van socialisatie voor beginnende studenten binnen het OPEN-ICT te bevorderen.
+
+## LET OP 
+- WIP 
+- DIT IS ALLEEN HET BACKEND
+## Ontwikkelaars
+
+- [@fszzl21](https://www.github.com/fszzl21)
+- [@boi-one](https://github.com/boi-one)
+- [@babacho](https://github.com/babacho)
